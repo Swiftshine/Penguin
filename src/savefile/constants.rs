d88a395e272e781f0f0bd0f0ce8e6803a02c459c
Swift <swiftshineexists@gmail.com>
2024-11-16 09:32:24 -0600

    SaveSlot::blank

--- src/savefile/constants.rs
+++ src/savefile/constants.rs
@@ -1,3 +1,6 @@
+use bitflags::bitflags;
+
+#[derive(Copy, Clone)]
 pub enum PlayerPowerup {
     None,
     Mushroom,
@@ -10,6 +13,8 @@ pub enum PlayerPowerup {
 
 pub const POWERUP_COUNT: usize = 7;
 
+
+#[derive(Copy, Clone)]
 pub enum PlayerCharacter {
     Mario,
     Luigi,
@@ -37,3 +42,37 @@ pub const AMBUSH_ENEMY_COUNT: usize = 4;
 // there are 64 hint movies, but 70 is the constant
 pub const HINT_MOVIE_COUNT: usize = 70;
 pub const ACTUAL_HINT_MOVIE_COUNT: usize = 64;
+
+#[derive(Copy, Clone)]
+pub enum StartingMushroomKind {
+    None,
+    Star,
+    Item,
+    OneUp,
+    StarRescue,
+    RedRescue,
+    GreenRescue,
+}
+
+bitflags!{
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct GameCompletionFlags: u8 {
+        const SaveEmpty           = 0b00000001;
+        const FinalBossBeaten     = 0b00000010;
+        const AllGoals            = 0b00000100;
+        const AllStarCoinsReg     = 0b00001000;
+        const AllStarCoinsSpe     = 0b00010000;
+        const GameCompleted       = 0b00100000;
+        const SuperGuideTriggered = 0b01000000;
+    }
+    
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct PlayerCreationFlags: u8 {
+        const None = 0;
+        const StarPower  = 0b00000001;
+        const Yoshi      = 0b00000010;
+        const Bubble     = 0b00000100;
+        const RescueToad = 0b00001000;
+    }
+}
+
